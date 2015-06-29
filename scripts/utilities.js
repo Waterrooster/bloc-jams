4d7cd3b63c22396d6b9fa3439e7d0d251166599e
Srikanth <srikanth.jonnakuti@hotmail.com>
2015-06-29 17:27:52 -0400

    Added utilities.js filr in scripts and added forEach loop in utilities.js

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,6 @@
 
 
+
 window.onload = function()
 {
 	var pointsArray = document.getElementsByClassName('point');
@@ -17,16 +18,17 @@ window.onload = function()
 	});
 var animatePoints = function(points)
 {
-	var revealPoint = function(i)
+	var animatePoints = function(points)
+{
+	
+	pointsArray.forEach(function(i)
 	{
 		points[i].style.opacity = 1;
 		points[i].style.transform = 'scaleX(1) translateY(0)';
 		points[i].style.msTransform = 'scaleX(1) translateY(0)';
 		points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
-	}
-	for(var i = 0; i<points.length;i++)
-	{
-		revealPoint(i);
-	}
+	});
+	
+}
 }
 }
